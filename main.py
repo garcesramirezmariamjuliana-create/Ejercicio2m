674f9f1dd62c988f810b2c0aa818ac8edfb8a1f1
mariam <garcesramirezmariamjuliana@gmail.com>
2026-05-13 08:54:13 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hola bienvenido al cajero automatico")
+
+#este progragra simula un cajero automatico 
+
+sal = 1000  #saldo
+opc = 0  #opcion
+
+print("MENÚ DEL CAJERO")
+print("1. Consultar saldo")
+print("2. Depositar dinero")
+print("3. Retirar dinero")
+print("4. Salir")
